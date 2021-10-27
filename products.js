3bd3d2ab03ff4842a0f4cfcac5e956c6bae3a93b
NickyGuants <nicholasguantai528@gmail.com>
2021-10-27 21:38:58 +0300

    change image src

--- products.js
+++ products.js
@@ -6,7 +6,7 @@ const products = [
       instock: 43,
       description:
       "At 4ft 6in wide, double beds are a popular choice with couples and for solo sleepers who like a little extra space to stretch out in at night.",
-      imgSrc: "/media/bed1.jpg"
+      imgSrc: "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
     },
     {
       id: 1,
@@ -15,7 +15,7 @@ const products = [
       instock: 100,
       description:
       "At 5ft wide, a king size bed provides the luxury of extra space to move around in. From statement styles to tech and space saving options.",
-      imgSrc: "/media/bed2.jpeg"
+      imgSrc: "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
     },
     {
       id: 2,
@@ -24,7 +24,7 @@ const products = [
       instock: 50,
       description:
       "Measuring 6ft wide, super king beds are our largest size bed frame. From luxurious upholstered designs to complement your bedroom décor.",
-      imgSrc: "/media/bed3.jpeg"
+      imgSrc: "https://images.unsplash.com/photo-1584132905271-512c958d674a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
     }
   ];
 
